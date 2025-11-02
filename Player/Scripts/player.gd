19442994d87dfc573571db99ee2281abf2f12071
王闻昊 <wwh27791@ly.com>
2025-11-02 18:15:06 +0800

    Tile Maps

--- Player/Scripts/player.gd
+++ Player/Scripts/player.gd
@@ -11,8 +11,12 @@ func _ready() -> void:
 	state_machine.initialize(self)
 
 func _process(_delta: float) -> void:
-	direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
-	direction.y = Input.get_action_strength("down") - Input.get_action_strength("up")
+	#direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
+	#direction.y = Input.get_action_strength("down") - Input.get_action_strength("up")
+	direction = Vector2(
+		Input.get_axis("left", "right"),
+		Input.get_axis("up", "down")
+	).normalized()
 
 func _physics_process(_delta: float) -> void:
 	move_and_slide()
