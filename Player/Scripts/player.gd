81cadcfd72a45d389ab2c07d43991324ad758a49
王闻昊 <wwh27791@ly.com>
2025-11-02 15:58:48 +0800

    空闲和走路动画

--- Player/Scripts/player.gd
+++ Player/Scripts/player.gd
@@ -1,13 +1,57 @@
 class_name Player extends CharacterBody2D
 
+var cardinal_direction: Vector2 = Vector2.DOWN
+var direction: Vector2 = Vector2.ZERO
 var move_speed: float = 100.0
+var state: String = "idle"
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var sprite: Sprite2D = $Sprite2D
 
 func _process(_delta: float) -> void:
-	var direction: Vector2 = Vector2.ZERO
+	
 	direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
 	direction.y = Input.get_action_strength("down") - Input.get_action_strength("up")
 	
 	velocity = direction * move_speed
+	
+	if set_state() || set_direction():
+		update_animation()
 
 func _physics_process(_delta: float) -> void:
 	move_and_slide()
+
+func set_direction() -> bool:
+	var new_direction: Vector2 = cardinal_direction
+	if direction == Vector2.ZERO:
+		return false
+	if direction.y == 0:
+		new_direction = Vector2.LEFT if direction.x < 0 else Vector2.RIGHT
+	elif direction.x == 0:
+		new_direction = Vector2.UP if direction.y < 0 else Vector2.DOWN
+	if new_direction == cardinal_direction:
+		return false
+		
+	cardinal_direction = new_direction
+	sprite.flip_h = cardinal_direction == Vector2.LEFT
+	
+	return true
+
+func set_state() -> bool:
+	var new_state: String = "idle" if direction == Vector2.ZERO else "walk"
+	if new_state == state:
+		return false
+	state = new_state
+	return true
+
+func update_animation() -> void:
+	animation_player.play(state + "_" + animition_direction())
+	pass
+	
+func animition_direction() -> String:
+	if (cardinal_direction == Vector2.DOWN):
+		return "down"
+	elif (cardinal_direction == Vector2.UP):
+		return "up"
+	else:
+		return "side"
