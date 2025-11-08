ac5a24009624bd9719d13194074184faf8d1d32e
王闻昊 <wwh27791@ly.com>
2025-11-08 20:31:35 +0800

    场景过渡淡入淡出效果

--- 00_Globals/global_level_manager.gd
+++ 00_Globals/global_level_manager.gd
@@ -24,13 +24,13 @@ func load_new_level(
 	get_tree().paused = true
 	target_transition = _target_transition
 	position_offset = _position_offset
-	await get_tree().process_frame
+	await SceneTransition.fade_out()
 	
 	level_load_started.emit()
 	await get_tree().process_frame
 	
 	get_tree().change_scene_to_file(level_path)
-	await get_tree().process_frame
+	await SceneTransition.fade_in()
 	
 	get_tree().paused = false
 	await  get_tree().process_frame
