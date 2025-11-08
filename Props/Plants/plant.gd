e1ff2db98a9d844f0945ec703b138be438d9698a
王闻昊 <wwh27791@ly.com>
2025-11-08 20:13:01 +0800

    场景管理器与区域过渡

--- Props/Plants/plant.gd
+++ Props/Plants/plant.gd
@@ -3,5 +3,5 @@ class_name Plate extends Node2D
 func _ready() -> void:
 	$HitBox.damaged.connect(take_damage)
 
-func take_damage(_damage: int) -> void:
+func take_damage(_hurt_box: HurtBox) -> void:
 	queue_free()
