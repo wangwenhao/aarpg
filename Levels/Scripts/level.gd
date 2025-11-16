ab92fb8362c1a9d6e1029b549e39e34f540ecdcf
王闻昊 <wangwenhao@live.cn>
2025-11-16 13:14:25 +0800

    场景音乐控制

--- Levels/Scripts/level.gd
+++ Levels/Scripts/level.gd
@@ -1,12 +1,13 @@
 class_name Level extends Node2D
 
+@export var music: AudioStream 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	self.y_sort_enabled = true
 	PlayerManager.set_as_parent(self)
 	LevelManager.level_load_started.connect(free_level)
-
+	AudioManager.play_music(music)
 
 func free_level() -> void:
 	PlayerManager.unparent_player(self)
