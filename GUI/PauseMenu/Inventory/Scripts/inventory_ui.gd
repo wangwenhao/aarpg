29cc06fa53932a7debceab1a55a3e5040ad47c40
王闻昊 <wangwenhao@live.cn>
2025-11-19 20:24:57 +0800

    修复背包重复的bug

--- GUI/PauseMenu/Inventory/Scripts/inventory_ui.gd
+++ GUI/PauseMenu/Inventory/Scripts/inventory_ui.gd
@@ -18,6 +18,7 @@ func clear_inventory() -> void:
 
 
 func update_inventory(index: int = 0) -> void:
+	clear_inventory()
 	for slot in data.slots:
 		var new_slot = INVENTORY_SLOT.instantiate()
 		add_child(new_slot)
