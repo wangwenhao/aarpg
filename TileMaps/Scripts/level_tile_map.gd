a70b1eccd61ef05f224e943895a9bb092a92915f
王闻昊 <wangwenhao@live.cn>
2025-11-29 21:58:55 +0800

    tilemaplayer

--- TileMaps/Scripts/level_tile_map.gd
+++ TileMaps/Scripts/level_tile_map.gd
@@ -1,5 +1,7 @@
 class_name LevelTileMap extends TileMapLayer
 
+@export var tile_size: float = 32
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	LevelManager.change_tile_map_bounds(get_tile_map_bounds())
@@ -7,9 +9,9 @@ func _ready() -> void:
 func get_tile_map_bounds() -> Array[Vector2]:
 	var bounds: Array[Vector2] = []
 	bounds.append(
-		Vector2(get_used_rect().position * rendering_quadrant_size)
+		Vector2(get_used_rect().position * tile_size) + position
 	)
 	bounds.append(
-		Vector2(get_used_rect().end * rendering_quadrant_size)
+		Vector2(get_used_rect().end * tile_size) + position
 	)
 	return bounds
